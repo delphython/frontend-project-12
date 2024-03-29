89b0bb484cd8fc84d9742e65123552fd5bf75027
Vitaly Klyukin <delphython@gmail.com>
2024-03-29 15:40:54 +0300

    add routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,19 @@
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from 'react-router-dom';
+import LoginPage from './Components/LoginPage';
+import MainPage from './Components/MainPage';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+          <Route path="/" element={<MainPage/> } />
+          <Route path="login" element={<LoginPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
