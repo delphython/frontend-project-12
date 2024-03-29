2638fcf593ae2ad626403364def0b2f2fc343092
Vitaly Klyukin <delphython@gmail.com>
2024-03-29 16:40:46 +0300

    add LoginPage

---
+++ frontend/src/components/LoginPage.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Formik, Form, Field } from 'formik';
+import * as Yup from 'yup';
+
+const SignupSchema = Yup.object().shape({
+  login: Yup.string()
+    .min(2, 'Минимум 2 буквы')
+    .max(50, 'Максимум 50 букв')
+    .required('Обязательное поле'),
+  password: Yup.string()
+    .min(2, 'Минимум 2 буквы')
+    .max(50, 'Максимум 50 букв')
+    .required('Обязательное поле'),
+});
+
+export const ValidationSchema = () => (
+  <div>
+    <h1>Signup</h1>
+    <Formik
+      initialValues={{
+        login: '',
+        password: '',
+      }}
+      validationSchema={SignupSchema}
+      onSubmit={ (values) => {
+        console.log(values);
+      }}
+    >
+      {({ errors, touched }) => (
+        <Form>
+          <Field name="login" />
+          {errors.login && touched.login ? (
+            <div>{errors.login}</div>
+          ) : null}
+          <Field name="password" />
+          {errors.password && touched.password ? (
+            <div>{errors.password}</div>
+          ) : null}
+          <button type="submit">Submit</button>
+        </Form>
+      )}
+    </Formik>
+  </div>
+);
