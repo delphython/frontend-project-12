04fcefd3ff63a2410b6b1be04fcebf588685e2c0
Vitaly Klyukin <delphython@gmail.com>
2024-03-30 09:13:50 +0300

    add MainPage

---
+++ frontend/src/components/MainPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+ 
+const MainPage = () => {
+    return (
+        <div>
+            <h1>Main Page</h1>
+            <h1>Page Found</h1>
+        </div>
+    );
+};
+ 
+export default MainPage;
