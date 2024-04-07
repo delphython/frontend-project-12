593fd99ad20819cc4cd0b65f027220a082232b96
Vitaly Klyukin <delphython@gmail.com>
2024-04-07 15:33:45 +0300

    add init

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import App from './components/App.jsx';
+import init from './init.jsx';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+const app = () => {
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+  const vdom = init();
+  root.render(<React.StrictMode>{vdom}</React.StrictMode>);
+};
+
+app();
