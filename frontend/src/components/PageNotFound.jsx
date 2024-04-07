30f1012f59829206f97dca5a885729df5879725c
Vitaly Klyukin <delphython@gmail.com>
2024-04-07 14:21:51 +0300

    update PageNotFound

--- frontend/src/components/PageNotFound.jsx
+++ frontend/src/components/PageNotFound.jsx
@@ -1,12 +1,16 @@
-import React from "react";
- 
-const PageNotFound = () => {
-    return (
-        <div>
-            <h1>404 Error</h1>
-            <h1>Page Not Found</h1>
-        </div>
-    );
-};
- 
+import React from 'react';
+
+import notFoundPic from '../assets/404.svg';
+
+const PageNotFound = () => (
+  <div className="text-center">
+    <img alt="Страница не найдена" className="img-fluid h-25" src={notFoundPic} style={{ width: 300 }} />
+    <h1 className="h4 text-muted">Страница не найдена</h1>
+    <p className="text-muted">
+      Но вы можете перейти
+      <a href="/"> на главную страницу</a>
+    </p>
+  </div>
+);
+
 export default PageNotFound;
