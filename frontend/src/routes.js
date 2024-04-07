9265c9e1bb2e1afb9ef0b53bb36d91b2a3c82b8c
Vitaly Klyukin <delphython@gmail.com>
2024-04-07 14:24:28 +0300

    update routes

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,6 +1,9 @@
 const apiPath = '/api/v1';
 
 // eslint-disable-next-line import/no-anonymous-default-export
-export default {
-  loginPath: () => [apiPath, 'login'].join('/'),
+const routes = {
+    loginPath: () => [apiPath, 'login'].join('/'),
+    dataPath: () => [apiPath, 'data'].join('/'),
 };
+
+export default routes;
