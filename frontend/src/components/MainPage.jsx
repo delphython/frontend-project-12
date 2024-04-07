fbc757ba522b035a18516ae2856bfc3ea690698e
Vitaly Klyukin <delphython@gmail.com>
2024-04-07 14:38:08 +0300

    add channels & messages

--- frontend/src/components/MainPage.jsx
+++ frontend/src/components/MainPage.jsx
@@ -1,26 +1,31 @@
 import axios from 'axios';
 import React, { useEffect } from 'react';
-
 import useAuth from '../hooks/index.js';
 import routes from '../routes.js';
+import { Container, Row } from 'react-bootstrap';
+import Channels from './Channels.jsx';
+import Messages from './Messages.jsx';
 
 const MainPage = () => {
 
-  const { getAuthHeader } = useAuth();
+    const { getAuthHeader } = useAuth();
+    
+    useEffect(() => {
+      const fetchData = async () => {
+        const response = await axios.get(routes.dataPath(), { headers: getAuthHeader() });
+        console.log(response.data);
+      };
+      fetchData();
+    }, [getAuthHeader]);
   
-  useEffect(() => {
-    const fetchData = async () => {
-      const response = await axios.get(routes.dataPath(), { headers: getAuthHeader() });
-      console.log(response.data);
-    };
-    fetchData();
-  }, [getAuthHeader]);
-
-  return (
-    <div>
-        Page Found
-    </div>
-  );
-};
+    return (
+      <Container className="h-100 my-4 overflow-hidden rounded shadow">
+        <Row className="h-100 bg-white flex-md-row">
+          <Channels />
+          <Messages />
+        </Row>
+      </Container>
+    );
+  };
 
 export default MainPage;
