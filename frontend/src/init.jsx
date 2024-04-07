5b64c67ba24b447a503cbd62b5b137cb9b891cb7
Vitaly Klyukin <delphython@gmail.com>
2024-04-07 21:35:17 +0300

    init locales

--- frontend/src/init.jsx
+++ frontend/src/init.jsx
@@ -4,11 +4,14 @@ import {
   useDispatch,
   useSelector,
 } from 'react-redux';
+import i18next from 'i18next';
+import { I18nextProvider, initReactI18next } from 'react-i18next';
 import store from './slices/index.js';
 import App from './components/App.jsx';
 import { SocketContext } from './contexts/index.js';
 import { actions as messagesActions } from './slices/messagesSlice.js';
 import { actions as channelsActions } from './slices/channelsSlice.js';
+import resources from './locales/index.js';
 
 const SocketProvider = ({ socket, children }) => {
   const dispatch = useDispatch();
@@ -82,11 +85,22 @@ const SocketProvider = ({ socket, children }) => {
   );
 };
 
-const init = (socket) => {
+const init = async (socket) => {
+  const i18n = i18next.createInstance();
+
+  await i18n
+    .use(initReactI18next)
+    .init({
+      resources,
+      fallbackLng: 'ru',
+    });
+
   return (
     <StoreProvider store={store}>
       <SocketProvider socket={socket}>
-        <App />
+        <I18nextProvider i18n={i18n}>
+          <App />
+        </I18nextProvider>
       </SocketProvider>
     </StoreProvider>
   );
