d228bd4bb7e65ccd1472e71f226efecb358ee4cc
Vitaly Klyukin <delphython@gmail.com>
2024-04-07 13:06:52 +0300

    rebuild LoginPage

--- frontend/src/components/LoginPage.js
+++ frontend/src/components/LoginPage.js
@@ -1,59 +1,83 @@
-import React from 'react';
-import { Formik, Form, Field } from 'formik';
+import axios from 'axios';
+import React, { useEffect, useRef, useState } from 'react';
+import { useFormik } from 'formik';
+import { Button, Form } from 'react-bootstrap';
 import * as Yup from 'yup';
 
-const SignupSchema = Yup.object().shape({
-  login: Yup.string()
+const validationSchema = Yup.object().shape({
+  username: Yup.string()
     .min(2, 'Минимум 2 буквы')
     .max(50, 'Максимум 50 букв')
     .required('Обязательное поле'),
   password: Yup.string()
-    .min(2, 'Минимум 2 буквы')
+    .min(5, 'Минимум 5 букв')
     .max(50, 'Максимум 50 букв')
     .required('Обязательное поле'),
 });
 
-const LoginPage = () => (
-  <div>
-    <h1>Signup</h1>
-    <Formik
-      initialValues={{
-        login: '',
-        password: '',
-      }}
-      validationSchema={SignupSchema}
-      onSubmit={({ setSubmitting }) => {
-        console.log("Form is validated! Submitting the form...");
-        setSubmitting(false);
-      }}
-    >
-      {({ errors, touched }) => (
-        <Form>
-          <div className="form-group">
-            <Field 
-              type="login"
-              name="login"
-              className="form-control" 
-            />
-            {errors.login && touched.login ? (
-              <div>{errors.login}</div>
-            ) : null}
-          </div>
-          <div className="form-group"> 
-            <Field
-              type="password"
-              name="password"
-              className="form-control"
-            />
-            {errors.password && touched.password ? (
-              <div>{errors.password}</div>
-            ) : null}
-          </div>
-          <button type="submit">Submit</button>
-        </Form>
-      )}
-    </Formik>
-  </div>
-);
+const LoginPage = () => {
+  const [authFailed, setAuthFailed] = useState(false);
+  const inputRef = useRef();
+
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
+
+  const formik = useFormik({
+    initialValues: {
+      username: '',
+      password: '',
+    },
+    validationSchema,
+    onSubmit: (values) => {
+      try {
+        setAuthFailed(false);
+        console.log(values);
+      } catch (err) {
+        setAuthFailed(true);
+        inputRef.current.select();
+        console.log(err);
+      }
+    },
+  });
+
+  return (
+    <Form onSubmit={formik.handleSubmit}>
+      <h1 className="text-center mb-4">Войти</h1>
+      <Form.Group>
+        <Form.Control
+          onChange={formik.handleChange}
+          value={formik.values.username}
+          name="username"
+          id="username"
+          autoComplete="username"
+          isInvalid={authFailed}
+          required
+          ref={inputRef}
+          placeholder="username"
+        />
+        <Form.Label htmlFor="username">Логин</Form.Label>
+      </Form.Group>
+      <Form.Group>
+        <Form.Control
+          onChange={formik.handleChange}
+          value={formik.values.password}
+          id="password"
+          isInvalid={authFailed}
+          placeholder="password"
+          name="password"
+          autoComplete="current-password"
+          type="password"
+          required
+        />
+        <Form.Label htmlFor="password">Пароль</Form.Label>
+        <Form.Control.Feedback type="invalid">
+          Неверные имя пользователя или пароль
+        </Form.Control.Feedback>
+      </Form.Group>
+      <Button type="submit" variant="outline-primary">Войти</Button>
+    </Form>
+  );
+};
 
 export default LoginPage;
