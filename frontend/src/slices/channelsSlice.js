5f7a20412039260dda8c864ae6ee1052ce78e2a6
Vitaly Klyukin <delphython@gmail.com>
2024-04-07 14:51:23 +0300

    add channels & messages slices

---
+++ frontend/src/slices/channelsSlice.js
@@ -0,0 +1,23 @@
+import { createSlice, createEntityAdapter } from '@reduxjs/toolkit';
+
+const channelsAdapter = createEntityAdapter();
+
+const initialState = channelsAdapter.getInitialState({
+    currentChannelId: null,
+});
+
+const channelsSlice = createSlice({
+  name: 'channels',
+  initialState,
+  reducers: {
+    addChannel: channelsAdapter.addOne,
+    addChannels: channelsAdapter.addMany,
+    setCurrentChannelId: ((state, action) => {
+        state.currentChannelId = action.payload;
+    }),
+  },
+});
+
+export const { actions } = channelsSlice;
+export default channelsSlice.reducer;
+export const selectors = channelsAdapter.getSelectors((state) => state.channels);
