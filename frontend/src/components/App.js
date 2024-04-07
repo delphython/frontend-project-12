1604c3c06a4cc664b520cf38f9f6f1b29353edd8
Vitaly Klyukin <delphython@gmail.com>
2024-04-07 13:43:12 +0300

    rebuild App

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,32 +1,31 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import {
   BrowserRouter,
   Routes,
   Route,
 } from 'react-router-dom';
-import LoginPage from './LoginPage';
-import MainPage from './MainPage';
-import PageNotFound from './PageNotFound';
+
+import LoginPage from './LoginPage.jsx';
+import MainPage from './MainPage.jsx';
+import NotFoundPage from './NotFoundPage.jsx';
 
 const App = () => {
-  const [token, setToken] = useState('');
-
-  useEffect(() => {
+  const hasToken = () => {
     const user = JSON.parse(localStorage.getItem('user'));
     if (user && user.token) {
-      setToken(user.token);
-    }
-  });
-
+      return true;
+    } return false;
+  };
+  
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={token ? <MainPage /> : <LoginPage />} />
-        <Route path="login" element={<LoginPage />} />
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={hasToken() ? <MainPage /> : <LoginPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
+  </BrowserRouter>
   );
-}
+};
 
 export default App;
