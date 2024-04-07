6c3345ce985bdbcca84481c3b719d53857405f72
Vitaly Klyukin <delphython@gmail.com>
2024-04-07 16:26:18 +0300

    realize renaming of the channel

--- frontend/src/slices/channelsSlice.js
+++ frontend/src/slices/channelsSlice.js
@@ -19,6 +19,7 @@ const channelsSlice = createSlice({
       const { id } = action.payload;
       channelsAdapter.removeOne(state, id);
     }),
+    changeChannelName: channelsAdapter.updateOne,
   },
 });
 
