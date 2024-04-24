58699b47d723bd2fc3657c9b359cc530f385f701
Vitaly Klyukin <delphython@gmail.com>
2024-04-24 11:27:39 +0300

    fix alt

--- frontend/src/components/PageNotFound.jsx
+++ frontend/src/components/PageNotFound.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import notFoundPic from '../assets/404.svg';
+import { routes } from '../routes.js';
 
 const PageNotFound = () => {
   const { t } = useTranslation();
 
   return (
     <div className="text-center">
-      <img alt="Страница не найдена" className="img-fluid h-25" src={notFoundPic} />
+      <img alt={t('notFound.header')} className="img-fluid h-25" src={notFoundPic} />
       <h1 className="h4 text-muted">{t('notFound.header')}</h1>
       <p className="text-muted">
         {t('notFound.message')}
